6561cf114d154a62d4c3eac1c682111d59f1ddaa
iLexGit <47642291+iLexGit@users.noreply.github.com>
2020-06-21 15:42:40 +0200

    Add files via upload

---
+++ PICtris - Alumnes/src/Alumne.java
@@ -0,0 +1,35 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+public class Alumne {
+
+    public static final int TIME_WAIT_MS = 10;
+
+    public static void sendFramebuffer(PICtris pictris, SerialPort serialPort) {
+        byte[] framebuffer = pictris.getFrameBuffer();
+
+        // Write your code here
+
+    }
+
+    public static void getInput(PICtris pictris, SerialPort serialPort) {
+        /* Example code:
+           pictris.move(-1); // Moves the current piece 1 block to the left
+           pictris.move(1); // Moves the current piece 1 block to the right
+           pictris.rotate(); // Rotates the current piece
+           pictris.userDropDown(); // Drops the current piece 1 position
+           pictris.hardDrop(); // Drops the current piece to bottom
+         */
+
+
+        // Write your code here
+
+    }
+
+
+    public static void gameOver(PICtris pictris, SerialPort serialPort) {
+        int score = pictris.getScore();
+
+        // Write your code here
+
+    }
+}
